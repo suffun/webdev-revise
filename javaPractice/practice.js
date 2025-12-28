88782bd0dea6ed829792de21b35e1b2ebaa6bebc
sufiyan khan <suffunew@gmail.com>
2025-12-28 22:24:48 +0530

    nested loop

--- javaPractice/practice.js
+++ javaPractice/practice.js
@@ -1,3 +1,18 @@
 let pencilPrice = 20;
 let eraserPrice = 10;
 console.log("The Total Price is", pencilPrice + eraserPrice,"rupees");
+console.log(`when we multiply both ${pencilPrice * eraserPrice}`)
+
+// nested if else statement 
+let marks = 80;
+if(marks>=35){
+    console.log("Student is passed");
+    if(marks>=80){
+        console.log("Student got O grade");
+    }else{
+        console.log("Student got A grade");
+    }
+}
+else{
+    console.log("student is failed");
+}
