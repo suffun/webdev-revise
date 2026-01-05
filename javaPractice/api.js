8eb8f6ac009eecb15a18a59826b6507d88c725ca
sufiyan khan <suffunew@gmail.com>
2026-01-05 22:49:44 +0530

    cat fact api

--- javaPractice/api.js
+++ javaPractice/api.js
@@ -12,15 +12,37 @@
 //     console.log("Found ERR",err);
 // });
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
+// async function getFacts(){
+//    try{
+//      let res = await fetch(url);
+//     let data  = await res.json();
+//     console.log(data.fact);
+//    }catch(err){
+//     console.log("Got error", err);
+
+//    }
+// };
+
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async()=>{
+   let fact = await getFacts();
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+});
+
+let url = "https://catfact.ninja/fact";
 async function getFacts(){
    try{
-     let res = await fetch(url);
-    let data  = await res.json();
-    console.log(data.fact);
+     let res = await axios.get(url);
+    return res.data.fact;
    }catch(err){
     console.log("Got error", err);
+    return "No fact Found";
 
    }
 };
