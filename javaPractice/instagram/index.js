7715271d66530c282f8f3a60a728a753f4cd5988
sufiyan khan <suffunew@gmail.com>
2026-01-06 22:45:18 +0530

    add data

--- javaPractice/instagram/index.js
+++ javaPractice/instagram/index.js
@@ -3,10 +3,22 @@ const path = require("path");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
-let port = 8080;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/",(req,res) => {
+  res.render("home");
+});
+
+app.get("/ig/:username", (req,res)=>{
+    let {username} = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    res.render("instagram",{data });
+});
+
+let port = 3000;
 app.listen(port, () => {
   console.log(`Server is runnning on ${port}`);
 });
