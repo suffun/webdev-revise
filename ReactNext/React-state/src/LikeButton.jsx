0ff307940497f7b1e6bca17d9a8162bd4d1599eb
sufiyan khan <suffunew@gmail.com>
2026-05-25 23:04:45 +0530

    like button

--- ReactNext/React-state/src/LikeButton.jsx
+++ ReactNext/React-state/src/LikeButton.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 export function LikeButton() {
     let [liked, isLiked] = useState(false);
     let toggleLike = () => {
-        isLiked(!liked);
+        isLiked(!liked); 
     }
+    let likeStyle = {color : "red"};
     return(
         <div>
              <p onClick={toggleLike}>
-                {liked ? (<i class="fa-solid fa-heart"></i>) : (<i class="fa-regular fa-heart"></i>)}
+                {liked ? (<i class="fa-solid fa-heart" style={likeStyle}></i>) : (<i class="fa-regular fa-heart"></i>)}
 
                 
             </p>
